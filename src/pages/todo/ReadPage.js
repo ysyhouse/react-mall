9b93682798f6314fa69e2e7bf06fd55b13d64c96
SunyoungYoon <ysyhouse@gmail.com>
2024-06-03 23:51:05 +0900

    readpage..

--- src/pages/todo/ReadPage.js
+++ src/pages/todo/ReadPage.js
@@ -1,15 +1,23 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useCallback } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 
 function ReadPage(props) {
     
-    const obj=useParams();
+    const {tno}=useParams();
 
-    console.log(obj)
+    const Navigate =useNavigate()
+
+    const moveToModify = useCallback((tno)=>{
+        Navigate({pathname:`/todo/modify/${tno}`})
+    },[tno])
     
     return (
         <div className='text-3xl font-extrabold'>
-            todo read page component
+            todo read page component {tno}
+        <div>
+        <button onClick={()=>moveToModify(33)}>Test Modify</button>
+        </div>
         </div>
     );
 }
