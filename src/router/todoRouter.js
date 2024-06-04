e67eaf89626e34f6d20e0ad4afbb497f1efb301b
SunyoungYoon <ysyhouse@gmail.com>
2024-06-04 21:23:41 +0900

    TodoModify

--- src/router/todoRouter.js
+++ src/router/todoRouter.js
@@ -6,6 +6,7 @@ const Loading= <div>Loading...</div>
 const TodoList= lazy(()=>import("../pages/todo/ListPage"))
 const TodoRead= lazy(()=>import("../pages/todo/ReadPage"))
 const TodoAdd= lazy(()=>import("../pages/todo/AddPage"))
+const TodoModify=lazy(()=>import("../pages/todo/ModifyPage"))
 
 const todoRouter=()=>{
     return[
@@ -24,6 +25,10 @@ const todoRouter=()=>{
             {
                 path:"add",
                 element:<Suspense fallback={Loading}><TodoAdd/></Suspense>
+            },
+            {
+                path:"modify/:tno",
+                element:<Suspense fallback={Loading}><TodoModify/></Suspense>
             },
 
     ]
