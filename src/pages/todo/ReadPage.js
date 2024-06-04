4e07e1072717b22ec8460a2376d79db3d9ccaf4f
SunyoungYoon <ysyhouse@gmail.com>
2024-06-04 21:06:55 +0900

    ..

--- src/pages/todo/ReadPage.js
+++ src/pages/todo/ReadPage.js
@@ -1,22 +1,39 @@
 import React, { useCallback } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
-import { Navigate } from 'react-router-dom';
+import { createSearchParams,useNavigate, useParams ,useSearchParams } from 'react-router-dom';
 
 function ReadPage(props) {
     
     const {tno}=useParams();
 
-    const Navigate =useNavigate()
+    const navigate =useNavigate()
+
+    const [queryParams] = useSearchParams()
+
+    const page= queryParams.get("page") ? parseInt(queryParams.get("page")) :1
+    const size= queryParams.get("size") ? parseInt(queryParams.get("size")) :10
+
+    const queryStr =createSearchParams({page,size}).toString()
+    
 
     const moveToModify = useCallback((tno)=>{
-        Navigate({pathname:`/todo/modify/${tno}`})
-    },[tno])
-    
+        navigate({pathname:`/todo/modify/${tno}`,
+        search : queryStr
+    })
+    },[tno,page,size])
+
+    const moveToList = useCallback(()=>{
+        navigate({pathname:`/todo/list`,
+        search : queryStr
+    })
+    },[page,size])
+
     return (
         <div className='text-3xl font-extrabold'>
             todo read page component {tno}
         <div>
-        <button onClick={()=>moveToModify(33)}>Test Modify</button>
+        <button onClick={()=>moveToModify(tno)}>Test Modify</button>
+        <button onClick={()=>moveToList()}>Test List</button>
+    
         </div>
         </div>
     );
